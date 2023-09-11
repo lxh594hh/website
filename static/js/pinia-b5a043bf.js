ca1a9fa33620e1503314c9304b5020b72480b120
lixiaohui <2216344711@qq.com>
2023-09-11 11:56:23 +0800

    v2

---
+++ static/js/pinia-b5a043bf.js
@@ -0,0 +1,7 @@
+import{ao as t,e as a,an as n}from"./@vue-3f1443a7.js";
+/*!
+ * pinia v2.1.6
+ * (c) 2023 Eduardo San Martin Morote
+ * @license MIT
+ */
+const s=Symbol();var c,e;function o(){const c=t(!0),e=c.run((()=>a({})));let o=[],r=[];const i=n({install(t){i._a=t,t.provide(s,i),t.config.globalProperties.$pinia=i,r.forEach((t=>o.push(t))),r=[]},use(t){return this._a?o.push(t):r.push(t),this},_p:o,_a:null,_e:c,_s:new Map,state:e});return i}(e=c||(c={})).direct="direct",e.patchObject="patch object",e.patchFunction="patch function";export{o as c};
